69ad12e8e37bde52998ddc8f93d50fc8d02ff55e
Farzaneh <f.mahmoodpoor@uconn.edu>
2020-06-03 20:17:19 -0400

    Project Euler Problem 01

--- P001.py
+++ P001.py
@@ -5,7 +5,8 @@
  we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000."""
 def multiples(x, y, z):
-    """ x, y, z -> int"""
+    """ x, y, z -> int
+    returnt all the multiples of y or z below x """
     sum = 0
     for i in range(x):
         if i%y == 0 or i%z == 0:
